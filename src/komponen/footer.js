411ea1485718b7d33443e4f3b51ef2642917b10e
itsqubeil <daffaiannabil28@gmail.com>
2023-07-09 22:12:30 +0700

    commit final

--- src/komponen/footer.js
+++ src/komponen/footer.js
@@ -104,7 +104,7 @@ function Footer() {
         </div>
 
         <p className="mt-8 text-base leading-6 text-center text-red-800">
-          © 2023 TugasBesar2, All rights reserved.
+          © 2023 Kelompok 8 Tugas Besar 2, All rights reserved.
         </p>
       </div>
     </section>
